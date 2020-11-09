859186fd4225573d8fd20cb309bc72c9c9ff8a07
Preston Bello <pbello24@biomedscienceacademy.org>
2020-11-09 13:31:47 +0000

    I wrote the rest of my story and changed some of my inouts and fixed spacing errors along with adding an f string.

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Preston Bello
+Period/Core: 3
 
 
 """
+print("Silly Stories")
+
+last_name = input ("Please input last_name")
+body_parts = input ("Please input body_parts")
+noun = input ("Please input noun")
+exclamation = input ("Please input exclamation")
+person_in_room = input("Please input person_in_room")
+adverb = input("Please input adverb")
+noun = input("Please input noun")
+adjective = input("Please input adjective")
+noun = input("Please input noun")
+exclamation = input("Please input exclamation")
+verb = input("Please input verb")
+noun = input("Please input noun")
+adjective = input("Please input adjective")
+
+print("There are a lot of rumors going around about Mr. " + last_name+" our chemistry teacher."  "\n He has always had a crazy look in his " + body_parts +"." "\n Sometimes, in the middle of a/an " + noun +"  lesson he'll shout out " + exclamation +"!" "\n My friend  " + person_in_room +" told me that he acts " + adverb +" because one time during a/an " + noun +" storm he was struck by lightning in his classroom." "\n Ouch! That would probably explain why he is so " + adjective +" all the time and shakes whenever he writes on the " + noun +"-board." "\n Last week, while doing an expierament in class, he yelled, " + exclamation +" It's alive! and then " + verb +" around the room holding a/an " + noun +" full of mysterious bubbling liquid." + f"\n Maybe the rumors are true; maybe my teacher really is {adjective} !")
